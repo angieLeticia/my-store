c3ba2393985e02688a44fd7e5f0205861f333eb6
Angie Gil <angieleticiagil@gmail.com>
2022-10-27 08:00:06 -0500

    Primera parte tienda DE ROPA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
+import{Product} from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,6 +19,43 @@ export class AppComponent {
 
  }
 
+ names: string[] = ['Angie','Andrea', 'Yuli', 'Paola'];
+ newName = '';
+
+  products: Product[] = [
+    {
+      name: 'Moda Europea',
+      price: 565,
+      image: './assets/img/1.jpg'
+    },
+    {
+      name: 'Moda Estadounidense',
+      price: 1002,
+      image: './assets/img/2.jpg'
+    },
+    {
+      name: 'Moda Colombiana',
+      price: 1326,
+      image: './assets/img/3.jpg'
+    },
+    {
+      name: 'Moda Africana',
+      price: 10002,
+      image: './assets/img/4.jpg'
+    },
+    {
+      name: 'Moda francesa',
+      price: 10012,
+      image: './assets/img/5.jpg'
+    },
+    {
+      name: 'Moda italiana',
+      price: 3002,
+      image: './assets/img/6.jpg'
+    }
+
+
+  ]
  toggleButton(){
   this.btnDisabled = !this.btnDisabled;
  }
@@ -31,5 +69,13 @@ console.log(element.scrollTop);
  changeName(event:Event){
   const element = event.target as HTMLInputElement;
   this.person.name = element.value;
+ }
+ addName(){
+  this.names.push(this.newName);
+  this.newName= '';
+ }
+ deleteName(index: number){
+  this.names.splice(index,1)
+
  }
 }
